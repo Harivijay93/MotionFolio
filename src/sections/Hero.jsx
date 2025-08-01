b5ad947ad4451c280e311ea4091c76375dd80ba2
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-01 18:16:02 +0530

    Add files via upload

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section id='Home' className='border border-amber-500'>
-        asssa
+    <section id='Home' className='h-dvh relative text-white-50 md:p-0 px-5'>
+        <div className='w-full h-full flex-center'>
+          <div className='container w-full h-full'>
+            <div className='md:mt-40 mt-20'>
+              <p className='font-medium md:text-2xl text-base'>Hey I'm here👋</p>
+              <h1 className='font-bold md:text-9xl text-5xl'>HARINATH</h1>
+              <h1 className='font-bold md:text-9xl text-5xl'>CREATIVE</h1>
+            </div>
+
+            <div className='absolute w-full z-30 bottom-20 right-0'>
+                <div className="flex justify-between items-end">
+                  <div className="flex flex-col items-center md:gap-5 gap-1">
+                    <p className='md:text-base text-xs'>Explore</p>
+                    <i class='bx bx-down-arrow-alt text-4xl animate-bounce'></i>
+                  </div>
+                    <div className='flex flex-col items-end'>
+                      <img src="public/images/shape.svg" alt="shape" />
+                      <h1 className='font-bold md:text-9xl text-5xl'>DEVELOPER</h1>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
     </section>
   )
 }
