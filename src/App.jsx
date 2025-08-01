d5c3ef7102bc12c17bf16bf1be3903b038abf2d5
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-01 15:56:26 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div>
+      <Hero />
+    </div>
   )
 }
 
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='Home' className='border border-amber-500'>
+        asssa
+    </section>
+  )
+}
+
+export default Hero
