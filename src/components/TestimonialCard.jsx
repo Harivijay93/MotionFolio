f540f9e326453352717d5669c3ca6c2735f0f184
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-07 11:17:42 +0530

    Add files via upload

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,35 +1,38 @@
 const TestimonialCard = ({ testimonial }) => {
   return (
-    <div className="col-span-1 p-10 bg-black-300 hover:bg-blue-300 rounded-xl
-    hover:-translate-y-3 transition-all duration-700 testimonial-card">
+    <div className="col-span-1 bg-black-300 testimonial-card group 
+    rounded-xl ">
+      <div className="group-hover:bg-blue-300 transition-all duration-700 p-10 rounded-xl">
       <div className="flex items-center justify-between">
         <img
           src={testimonial.imgPath}
           alt="cover-img"
-          className="md:w-36 md:h-36 w-24 h-24"
+          className="md:w-36 md:h-36 w-24 h-24 border border-transparent rounded-full
+          group-hover:border-white group-hover:translate-y-2 group-hover:translate-x-2 transition-all duration-700"
         />
         <img
           src="/images/quote.png"
           alt="cover-img"
-          className="md:w-28 md:h-28 w-14 h-14"
+          className="md:w-28 md:h-28 w-14 h-14 group-hover:mix-blend-screen transition-all duration-700"
         />
       </div>
       <div className="mt-10">
-        <p className="md:text-2xl font-light">{testimonial.review}</p>
+        <p className="md:text-2xl font-light group-hover:text-white-50 transition-all duration-700">{testimonial.review}</p>
       </div>
       <div className="flex md:flex-row flex-col md:items-center justify-between md:mt-20 mt-10">
         <div>
-          <h1 className="text-blue-50 md:text-3xl text-lg font-medium mb-1">
+          <h1 className="text-blue-50 md:text-3xl text-lg font-medium mb-1 group-hover:text-white transition-all duration-700">
             {testimonial.name}
           </h1>
-          <p className="md:text-xl text-sm opacity-70 font-light">
+          <p className="md:text-xl text-sm opacity-70 font-light group-hover:text-white-50 transition-all duration-700">
             {testimonial.pos}
           </p>
         </div>
         <div className="flex items-center gap-3 md:mt-0 mt-5">
-          <i class='bx bxl-whatsapp md:text-5xl text-2xl'></i>
-          <i class='bx bxl-instagram md:text-5xl text-2xl'></i>
+          <i class='bx bxl-whatsapp md:text-5xl text-2xl  group-hover:text-white-50 transition-all duration-700'></i>
+          <i class='bx bxl-instagram md:text-5xl text-2xl  group-hover:text-white-50 transition-all duration-700'></i>
         </div>
+      </div>
       </div>
     </div>
   );
