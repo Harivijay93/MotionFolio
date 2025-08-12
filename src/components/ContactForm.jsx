e33c3ca767881e7291dcab8d290cd68be7698ab3
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-12 11:19:26 +0530

    Add files via upload

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,38 +5,39 @@ const ContactForm = () => {
     <div className='flex-center'>
         <form className="w-full text-[#a7a7a7] flex flex-col gap-7">
             <div className="">
-                <label  className="block text-white md:text-2xl font-semibold mb-2"
+                <label  className="label"
                         htmlFor="name">Name</label>
                 <input type="text"
                        id="name"
                        placeholder="Enter your name"
-                       className="w-full px-4 py-4 font-light md:text-base text-sm placeholder:text-[#fafafa50] bg-black-300 rounded-md"/>
+                       className="input"/>
             </div>
             <div className="">
-                <label  className="block text-white md:text-2xl font-semibold mb-2"
+                <label  className="label"
                         htmlFor="name">Email Address</label>
                 <input type="text"
                        id="name"
                        placeholder="Enter your email"
-                       className="w-full px-4 py-4 font-light md:text-base text-sm placeholder:text-[#fafafa50] bg-black-300 rounded-md"/>
+                       className="input"/>
             </div>
             <div className="">
-                <label  className="block text-white md:text-2xl font-semibold mb-2"
+                <label  className="label"
                         htmlFor="name">Subject</label>
                 <input type="text"
                        id="name"
                        placeholder="Enter your subject"
-                       className="w-full px-4 py-4 font-light md:text-base text-sm placeholder:text-[#fafafa50] bg-black-300 rounded-md"/>
+                       className="input"/>
             </div>
             <div className="">
-                <label  className="block text-white md:text-2xl font-semibold mb-2"
+                <label  className="label"
                         htmlFor="name">Message</label>
                 <textarea type="text"
                        id="name"
                        placeholder="Enter your message"
                        rows={"5"}
-                       className="w-full px-4 py-4 font-light md:text-base text-sm placeholder:text-[#fafafa50] bg-black-300 rounded-md"/>
+                       className="input"/>
             </div>
+            <button type='submit' className='cursor-pointer w-full py-4 bg-blue-50 text-white-50 font-semibold rounded-md hover:bg-blue-300 transition-all duration-300'>Send Message</button>
         </form>
     </div>
   )
